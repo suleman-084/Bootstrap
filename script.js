2277112d11256076b5a1fe63db681bb2b454975b
mohammed suleman <sallusk084@gmail.com>
2023-09-20 16:01:11 +0530

    api is posted

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 const slider = document.querySelector("input");
 const value = document.querySelector(".value");
 const cards = document.querySelectorAll(".pricing-card");
+const submitButton = document.querySelector('button[type="submit"]');
+const form = document.getElementById("myForm");
+
+const submit = document.getElementById("sub");
+
+submit.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById("fname").value;
+  const email = document.getElementById("email").value;
+  const comments = document.getElementById("comments").value;
+  console.log("name", name, "email", email, "comments", comments);
+  await fetch(
+    "https://cors-anywhere.herokuapp.com/https://forms.maakeetoo.com/formapi/638",
+    {
+      method: "POST",
+
+      body: JSON.stringify({
+        firstname: name,
+        email: email,
+        message: comments,
+      }),
+
+      headers: {
+        "Content-type": "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+
+  form.addEventListener("submit", function (event) {
+    const nameInput = document.getElementById("fname");
+    const emailInput = document.getElementById("email");
+    const commentsInput = document.getElementById("comments");
+
+    if (!nameInput.value || !emailInput.value || !commentsInput.value) {
+      event.preventDefault();
+      alert("Please fill in all required fields.");
+    } else if (!isValidEmail(emailInput.value)) {
+      event.preventDefault();
+      alert("Please enter a valid email address (e.g., email@gmail.com).");
+    }
+  });
+
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+});
 
 value.textContent = slider.value;
 
